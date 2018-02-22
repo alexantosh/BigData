1e6ac5a262fe13e2dc2f63d3753fbf6af2749b99
Alex <alexantosh@yahoo.com>
2018-02-22 14:25:37 +0300

    First R file and the data file

---
+++ First.R
@@ -0,0 +1,44 @@
+#Data Types
+
+#Numeric Data Types
+myNumeric <- c(0.5, 2.5) ## ===> myNumeric = c(0.5, 2.5)
+
+#Logical
+myLogical = c(TRUE, FALSE)
+myLogical2 = c(T,F)
+
+#Character
+myCharacter = c("a", "b", 'c', 'd')
+
+#Integers In R
+myIntSeq = 1:100
+
+#Complex Numbers
+myComplex = c(6+45i, 4+8i)
+
+#Vectors
+vect = vector("numeric", length = 10)
+
+vc
+
+seq2 = 0:6
+
+as.logical(seq2)
+as.numeric(seq)
+as.numeric_version(seq)
+as.logical(seq2)
+is.atomic(seq2)
+as.complex(seq2)
+as.null(seq2)
+class(vect)
+#.....
+
+mat = matrix(nrow = 5, ncol = 7, byrow = F, , names = list(c("One", "Two", "Three", "Four", "Five"), c("Name", "Surname", "Age", "Year", "Dead", "Alive", "Lost"))
+
+mat
+vect
+myCharacter
+myComplex
+myIntSeq
+myLogical
+myNumeric
